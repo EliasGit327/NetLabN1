a779e8700c714b2dd15403b3fcf6065c325f4467
Ellay <goodanonim96@gmail.com>
2019-01-31 12:36:56 +0200

    First

---
+++ src/Drawler.java
@@ -0,0 +1,25 @@
+public class Drawler
+{
+    public static void draw()
+    {
+        int width = 72;
+        int heigth = 20;
+
+        for(int i=0; i<width; i++)
+            System.out.print("#");
+
+        for(int x = 0; x<heigth; x++)
+        {
+            System.out.println("");
+            System.out.print("#");
+            for (int i = 0; i < width-2; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("#");
+        }
+        System.out.println("");
+
+        for(int i=0; i<width; i++)
+            System.out.print("#");
+    }
+}

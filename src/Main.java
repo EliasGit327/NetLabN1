c5dd4f9d5715a0859de16d1cf0a7a09abccb3f16
Ellay <goodanonim96@gmail.com>
2019-02-06 11:42:12 +0200

    Program's base functionality is done (02.02.2019 19:13)

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,49 @@
 import helpers.Helper;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        int width = 175;
+public class Main {
+    public static void main(String[] args) {
+        int width = 215;
         String newText = "";
 
-        URL url = Main.class.getResource("lycris.txt");
 
-        String fileText = Helper.readFromFile(url.getPath());
+//        URL url = Main.class.getResource("lycris.txt");
+//        String fileText = Helper.readFromFile(url.getPath());
+
+        String filePath = "/Users/ellay/Desktop/lycris.txt";
+        String fileText = Helper.readFromFile(filePath);
+
 
         String[] text = fileText.split("\n");
 
 
-        if( checkSize(text, width) )
-        {
+        if (checkSize(text, width)) {
             for (String t : text) {
                 newText = newText + Helper.getNewLine(t, width) + "\n";
             }
 
             System.out.print(newText);
-        }
-        else
-        {
+        } else {
             System.out.print("Text has a line or more which is/are wider than your width\nChange width parameter or text");
         }
     }
 
-    private static boolean checkSize(String[] text, int width)
-    {
+    private static boolean checkSize(String[] text, int width) {
         int maxSize = 0;
-        for (String t:text)
-        {
-            if (maxSize < t.length())
-            {
+        for (String t : text) {
+            if (maxSize < t.length()) {
                 maxSize = t.length();
             }
         }
 
         return maxSize <= width;
     }
+
 }
 
 
